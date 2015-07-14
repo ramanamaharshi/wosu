0a29aa4a754b18f44c58d4aee5420da1a87e5069
oliver <@>
2015-07-14 22:07:10 +0200

    maps: add custom markers

--- pages/map.php
+++ pages/map.php
@@ -10,25 +10,34 @@
 	
 	$sGoogleMaps = 'https://maps.googleapis.com/maps/api/js?v=3&sensor=false';
 	
+	$aMarkerIcon = array(
+		'url' => '/public/img/marker_icon_a.svg',
+		'scaledSize' => array(20,20),
+		'bounds' => array(5,0,10,20),
+		'origin' => array(5,20),
+	);
 	$aMarkers = array();
 	$aAds = Ad::oGet(array(), false);
 	foreach ($aAds as $oAd) {
 		$oCoords = $oAd->oAddress->oCoords;
 		$aMarker = array(
-			'sTitle' => 'test',
-			'nLat' => $oCoords->iY,
-			'nLon' => $oCoords->iX,
+			'title' => 'test',
+			'icon' => $aMarkerIcon,
+			'position' => array(
+				'nLat' => $oCoords->iY,
+				'nLon' => $oCoords->iX,
+			),
 		);
 		$aMarkers []= $aMarker;
 	}
 	$aOptions = array(
 		'zoom' => 13,
-		'type' => 'satelite',
+		'type' => 'satellite',
 		'center' => '50.7753222,6.0838673',
 	);
 	
 	$sMapHtml = Maps::sMakeMapHtml($aOptions, $aMarkers);
-	$sContent = '<h1>Karte</h1>' . $sMapHtml;
+	$sContent = '<!--h1>Karte</h1-->' . $sMapHtml;
 	
 	$sHeader = Html::sMakeHeader(array(), array());
 	$sFooter = Html::sMakeFooter(array());
--- public/js/maps.js
+++ public/js/maps.js
@@ -6,14 +6,22 @@
 	
 	var oParsePosition = function (mPosition) {
 		
-		if (typeof mPosition == 'string') {
-			mPosition = mPosition.split(',');
-		}
-		if (typeof mPosition == 'object' && typeof mPosition[0] != 'undefined') {
-			mPosition = {nLat: parseFloat(mPosition[0]), nLon: parseFloat(mPosition[1])};
+		if (!(typeof mPosition == 'object' && mPosition instanceof google.maps.LatLng)) {
+			if (typeof mPosition == 'string') {
+				mPosition = mPosition.split(',');
+			}
+			if (typeof mPosition == 'object') {
+				if (typeof mPosition[0] != 'undefined') {
+					mPosition = {iX: parseFloat(mPosition[1]) , iY: parseFloat(mPosition[0])};
+				}
+				if (typeof mPosition.iX != 'undefined') {
+					mPosition = {nLat: mPosition.iY , nLon: mPosition.iX};
+				}
+			}
+			mPosition = new google.maps.LatLng(mPosition.nLat , mPosition.nLon);
 		}
 		
-		return new google.maps.LatLng(mPosition.nLat,mPosition.nLon);
+		return mPosition;
 		
 	};
 	
@@ -36,14 +44,60 @@
 		
 		var oMap = new google.maps.Map(jThis[0], oOptions);
 		
-		for (var iM = 0; iM < aMarkers.length; iM ++) {
-			var oMarker = aMarkers[iM];
-			oMarker.oMarker = new google.maps.Marker({
-				map: oMap,
-				position: new google.maps.LatLng(oMarker.nLat,oMarker.nLon),
-				title: oMarker.sTitle,
-			});
+		oMap.oBindings = {};
+		oMap.vBind = function (sEvent, fCallback) {
+			if (typeof oMap.oBindings[sEvent] == 'undefined') {
+				oMap.oBindings[sEvent] = [];
+				google.maps.event.addListener(oMap, sEvent, function(){
+					for (var iB = 0; iB < oMap.oBindings[sEvent].length; iB ++) {
+						var f = oMap.oBindings[sEvent][iB];
+						f.call(oMap, sEvent, arguments);
+					}
+				});
+			}
+			oMap.oBindings[sEvent].push(fCallback);
 		}
+		
+		for (var iM = 0; iM < aMarkers.length; iM ++) {( function(){
+			
+			var oMarker = aMarkers[iM];
+			
+			oMarker.map = oMap;
+			if (typeof oMarker.position != 'undefined') {
+				oMarker.position = oParsePosition(oMarker.position);
+			}
+			if (typeof oMarker.icon != 'undefined') {
+				if (typeof oMarker.icon.bounds != 'undefined') {
+					oMarker.icon.origin = [oMarker.icon.bounds[0], oMarker.icon.bounds[1]];
+					oMarker.icon.size = [oMarker.icon.bounds[2], oMarker.icon.bounds[3]];
+				}
+				var oIconAttrs = {scaledSize: 'Size', origin: 'Point', size: 'Size', anchor: 'Point'};
+				for (var sAttr in oIconAttrs) {
+					var sClass = oIconAttrs[sAttr];
+					if (typeof oMarker.icon[sAttr] != 'undefined' && !(oMarker.icon[sAttr] instanceof google.maps[sClass])) {
+						oMarker.icon[sAttr] = new google.maps[sClass](oMarker.icon[sAttr][0],oMarker.icon[sAttr][1]);
+					}
+				}
+			}
+			
+			oMarker.oMarker = new google.maps.Marker(oMarker);
+			
+			oMarker.oMarker.oBindings = {};
+			oMarker.oMarker.vBind = function (sEvent, fCallback) {
+				var gMarker = oMarker.oMarker;
+				if (typeof gMarker.oBindings[sEvent] == 'undefined') {
+					gMarker.oBindings[sEvent] = [];
+					google.maps.event.addListener(gMarker, sEvent, function(){
+						for (var iB = 0; iB < gMarker.oBindings[sEvent].length; iB ++) {
+							var f = gMarker.oBindings[sEvent][iB];
+							f.call(oMarker, sEvent, arguments);
+						}
+					});
+				}
+				gMarker.oBindings[sEvent].push(fCallback);
+			};
+			
+		})(); }
 		
 		jThis.data('oMap', oMap);
 		
